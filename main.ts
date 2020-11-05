872de084603d27e668f029bf367616df491dce3f
crouse12 <crousekimo@yahoo.com.tw>
2020-11-05 11:12:20 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 //% weight=0 color=#FFBA3B icon="\uf0ad" block="microbitusarthmi"
 namespace microbitusarthmi {
          export enum color {
-        WHITE = "White",
-        BLACK = "BLACK"
+        WHITE = 1,
+        BLACK = 2
      }
     //% group="1.Basic"  
     //% blockId=cls block="Clear screen color %color1"
     //% weight=101
     export function cls(color1: color):void {
-       serial.writeLine("cls "+color1)
+       serial.writeLine("cls "+color1.toString())
        basic.pause(100)
     }
 }
