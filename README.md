ed996504e63a4f653467b6537359c9199c26605e
crouse12 <crousekimo@yahoo.com.tw>
2020-11-05 10:21:32 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# microbitusarthmi-1
+# microbitusarthmi
+
+Micro:Bit控制Usart Hmi積木
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+microbitusarthmi=github:crouse12/microbitusarthmi
+```
---
+++ main.ts
@@ -0,0 +1,10 @@
+//% weight=0 color=#FFBA3B icon="\uf0ad" block="microbitusarthmi"
+namespace microbitusarthmi {
+    //% group="1.Basic"  
+    //% blockId=cls block="Clear screen color %color"
+    //% weight=101
+    export function cls(color1: color):void {
+       serial.writeLine("cls "+color1)
+       basic.pause(100)
+    }
+}
